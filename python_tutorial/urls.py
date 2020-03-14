e29aab5e45875da0f4317ef93f5e130553de9ed2
sunayanaakula <sunayanaakula158@gmail.com>
2020-03-14 12:10:11 +0530

    static files fixed

--- python_tutorial/urls.py
+++ python_tutorial/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from tutorial import views
+from django.conf.urls.static import static
 
+
+from django.conf import settings
 urlpatterns = [
     # Invoke the home view in the tutorial app by default
     url(r'^$', views.home, name='home'),
     # Defer any URLS to the /tutorial directory to the tutorial app
     url(r'^tutorial/', include('tutorial.urls', namespace='tutorial')),
     url(r'^admin/', admin.site.urls),
-]
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
