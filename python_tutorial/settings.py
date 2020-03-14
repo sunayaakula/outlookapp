84ad1c4293cae1d557ed79b105399221ab7a372c
sunayanaakula <sunayanaakula158@gmail.com>
2020-03-14 12:52:36 +0530

    added media root

--- python_tutorial/settings.py
+++ python_tutorial/settings.py
@@ -102,5 +102,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+MEDIA_URL = '/assets/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'assets')
+
 # Activate Django-Heroku.
 django_heroku.settings(locals())
